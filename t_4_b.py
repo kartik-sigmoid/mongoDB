ced6663f53d84730161a29f69b9135fe556da422
Kartik Jaiswal <kartikjaiswal@AS-MAC-0284.local>
2022-02-07 18:47:27 +0530

    4.b done

--- t_4_b.py
+++ t_4_b.py
@@ -3,23 +3,11 @@ from pymongo import MongoClient
 
 def task_one(n, movies):
     pipeline = [
-        {
-            "$project":
-                {
-                    "_id": 0,
-                    "imdb.rating": 1,
-                    "title": 1
-                }
-        },
-        {
-            "$sort":
-                {
-                    "imdb.rating": -1
-                }
-        },
-        {
-            "$limit": n
-        }
+        {'$project': {'title': '$title', 'rating': '$imdb.rating'}},
+        {'$match': {'rating': {'$exists': True, '$ne': ''}}},
+        {'$group': {'_id': {'rating': '$rating', 'title': '$title'}}},
+        {'$sort': {'_id.rating': -1}},
+        {'$limit': n}
     ]
     li = movies.aggregate(pipeline)
     res = []
@@ -100,34 +88,130 @@ def task_six(n, year, movies):
     return res
 
 
+def task_seven(n, genres, movies):
+    pipeline = [
+        {"$unwind": "$directors"},
+        {"$unwind": "$genres"},
+        {"$group": {"_id": {"directors": "$directors", "genres": "$genres"}, "no_of_films": {"$sum": 1}}},
+        {"$sort": {"no_of_films": -1}},
+        {"$match": {"_id.genres": genres}},
+        {"$limit": n}
+    ]
+    li = movies.aggregate(pipeline)
+    res = []
+    for i in li:
+        res.append(i)
+    return res
+
+
+def task_eight(n, movies):
+    pipeline = [
+        {"$unwind": "$cast"},
+        {"$group": {"_id": {"cast": "$cast"}, "no_of_films": {"$sum": 1}}},
+        {"$sort": {"no_of_films": -1}},
+        {"$limit": n}
+    ]
+    li = movies.aggregate(pipeline)
+    res = []
+    for i in li:
+        res.append(i)
+    return res
+
+
+def task_nine(n, year, movies):
+    pipeline = [
+        {"$unwind": "$cast"},
+        {"$group": {"_id": {"cast": "$cast", "year": "$year"}, "no_of_films": {"$sum": 1}}},
+        {"$sort": {"no_of_films": -1}},
+        {"$match": {"_id.year": year}},
+        {"$project": {"_id.year": 0}},
+        {"$limit": n}
+    ]
+    li = movies.aggregate(pipeline)
+    res = []
+    for i in li:
+        res.append(i)
+    return res
+
+
+def task_ten(n, genres, movies):
+    pipeline = [
+        {"$unwind": "$cast"},
+        {"$unwind": "$genres"},
+        {"$group": {"_id": {"cast": "$cast", "genres": "$genres"}, "no_of_films": {"$sum": 1}}},
+        {"$sort": {"no_of_films": -1}},
+        {"$match": {"_id.genres": genres}},
+        {"$project": {"_id.genres": 0}},
+        {"$limit": n}
+    ]
+    li = movies.aggregate(pipeline)
+    res = []
+    for i in li:
+        res.append(i)
+    return res
+
+
+def task_eleven(n, movies, genres):
+    pipeline = [
+        {"$unwind": "$genres"},
+        {"$match": {"genres": genres, 'imdb.rating': {'$exists': True, '$ne': ''}}},
+        {"$project": {"_id": 0, "title": 1, 'rating': '$imdb.rating'}},
+        {'$group': {'_id': {'title': '$title', 'rating': '$rating'}}},
+        {"$sort": {"_id.rating": -1}},
+        {"$limit": n}
+    ]
+    li = movies.aggregate(pipeline)
+    res = []
+    for i in li:
+        res.append(i)
+    return res
+
 def queries(movies):
     n = int(input("Enter the value for N: "))
-    # taskOne = task_one(n, movies)
-    # print(taskOne)
+    taskOne = task_one(n, movies)
+    print(taskOne)
 
-    # year = int(input("Enter the year: "))
-    # taskTwo = task_two(n, year, movies)
-    # print(taskTwo)
+    year = int(input("Enter the year: "))
+    taskTwo = task_two(n, year, movies)
+    print(taskTwo)
 
-    # print("Top n movies with vote greater than 1000")
-    # taskThree = task_three(n, movies)
-    # print(taskThree)
+    print("Top n movies with vote greater than 1000")
+    taskThree = task_three(n, movies)
+    print(taskThree)
 
-    # print("Top n movies with movies title matching: ")
-    # taskFour = task_four(n, movies, "The")
-    # print(taskFour)
+    print("Top n movies with movies title matching: ")
+    taskFour = task_four(n, movies, "The")
+    print(taskFour)
 
-    # print('Top n directors with maximum no. of votes: ')
-    # taskFive = task_five(n, movies)
-    # print(taskFive)
+    print('Top n directors with maximum no. of votes: ')
+    taskFive = task_five(n, movies)
+    print(taskFive)
 
     print('Top directors who created maximum movies on given year')
     year = int(input("enter year: "))
     taskSix = task_six(n, year, movies)
     print(taskSix)
 
+    print("top directors in given genre")
+    taskSeven = task_seven(n, "Action", movies)
+    print(taskSeven)
 
+    print("top `n actors who starred in the maximum number of movies")
+    taskEight = task_eight(n, movies)
+    print(taskEight)
 
+    print("top n actors who starred in the maximum number of movies in a given year")
+    year = int(input("enter the year: "))
+    taskNine = task_nine(n,  year, movies)
+    print(taskNine)
+
+    print("top n actors who starred in the maximum number of movies for a given genre")
+    taskTen = task_ten(n, "Action", movies)
+    print(taskTen)
+
+    print("top n movies for each genre with the highest IMDB rating")
+    taskEleven = task_eleven(n, movies, "Action")
+    print(taskEleven)
 
 
 if __name__ == "__main__":
