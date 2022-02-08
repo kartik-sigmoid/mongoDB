cd6952404b4a7aa5ef93579baef9ac74b081d348
Kartik Jaiswal <kartikjaiswal@AS-MAC-0284.local>
2022-02-08 23:56:05 +0530

    geo near

--- task_4_c.py
+++ task_4_c.py
@@ -15,30 +15,6 @@ def task_one(theaters):
     return res
 
 
-def task_two(collections, coord):
-    pipeline = [
-     {
-         "$geoNear": {
-             "near": {"type": "Point", "coordinates": [-84.526169, 37.986019] },
-             "maxDistance":10*10000000000000,
-             "distanceField": "dist.calculated",
-             "includeLocs": "dist.location",
-             "distanceMultiplier":1/1000,
-             "spherical": "true"
-      }
-     },
-         {"$project": {"city": "$location.address.city", "distance": "$dist.calculated"}},
-         {"$group": {"_id": {"distance": "$distance", "city": "$city"} }},
-         {"$sort": {"_id.distance": 1}},
-         {"$limit": 10}
-    ]
-    li = collections.aggregate(pipeline)
-    res = []
-    for i in li:
-        res.append(i)
-    return res
-
-
 def task_two_2(theaters):
     pipeline2 = [
         {
